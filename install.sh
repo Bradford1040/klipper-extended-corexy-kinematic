917df0805d71dfe1da724e68ce65739b1b0170b8
𝔅яа∂ƒøя∂¹⁰⁴⁰ <bradfordaldenadams@gmail.com>
2025-10-23 02:02:50 -0400

    Change Klipper service restart command to klipper-punisher
    For those that have multiple klipper installs and custom named printers using KIAUH, you will notice when you use a install.sh from someone they never seem to remember that "sudo systemctl restart klipper" will not restart klipper, as each install of klipper has been modified to the custom name of the printer "klipper-(custom_name)"  
    
    Signed-off-by: 𝔅яа∂ƒøя∂¹⁰⁴⁰ <bradfordaldenadams@gmail.com>

--- install.sh
+++ install.sh
@@ -12,5 +12,5 @@ SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/ && pwd )"
 echo "Linking extended_corexy.py to Klipper kinematic directory"
 ln -sf "${SRCDIR}/src/extended_corexy.py" "${KLIPPER_PATH}/klippy/kinematics/extended_corexy.py"
 echo "Restarting Klipper..."
-sudo systemctl restart klipper
+sudo systemctl restart klipper-punisher
 
